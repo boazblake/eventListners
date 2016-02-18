718415bc301c16a9a6b54110b1087e7bdcce3e2c
Boaz Blake <boazblake@gmail.com>
2016-02-18 13:50:52 -0600

    updates

--- main.js
+++ main.js
@@ -1,22 +1,71 @@
 // GO!
 
 
+ var navBox = document.querySelector(".answer-box")
+ // console.log(navBox)
 
-// BONUS 
-var input = document.querySelector("#add-guest-bonus input")
-var guestList = document.querySelector("#add-guest-bonus ul")
+ // Append the following on to navPArentBox
+// <input id="navButt" value="Show/Hide Nav" type="button" />
 
-var addGuest = function(e) {
-	console.log(e.keyCode)
-	if (e.keyCode === 13) {
-		var newGuestItem = document.createElement('li')
-		newGuestItem.innerHTML = e.target.value + "<button>X</button>"
-		newGuestItem.classList.add('guest')
-		newGuestItem.querySelector('button').addEventListener('click',function() {
-			guestList.removeChild(newGuestItem)
-		})
-		guestList.appendChild(newGuestItem)
-		e.target.value = ''
-	}
-}
-input.addEventListener('keypress',addGuest)
+var navButton = document.createElement('button');
+navButton.innerHTML =
+ '<input id="navButt" value="Show/Hide Nav" type="button" />';
+
+navBox.appendChild(navButton);
+// console.log(navButton)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // BONUS 
+// var input = document.querySelector("#add-guest-bonus input")
+// var guestList = document.querySelector("#add-guest-bonus ul")
+
+// var addGuest = function(e) {
+// 	console.log(e.keyCode)
+// 	if (e.keyCode === 13) {
+// 		var newGuestItem = document.createElement('li')
+// 		newGuestItem.innerHTML = e.target.value + "<button>X</button>"
+// 		newGuestItem.classList.add('guest')
+// 		newGuestItem.querySelector('button').addEventListener('click',function() {
+// 			guestList.removeChild(newGuestItem)
+// 		})
+// 		guestList.appendChild(newGuestItem)
+// 		e.target.value = ''
+// 	}
+// }
+// input.addEventListener('keypress',addGuest)
